a439b45bce56787ac9fa3035c1332c1d68111f6a
Brian C. Lindner <brian@bclindner.com>
2018-10-26 16:54:28 -0400

    added .emote

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import libraries
 const Discord = require('discord.js') // discord bot framework
 const low = require('lowdb') // local JSON flat-file database
-
+const path = require('path') // for passing absolute paths to commands
 // set up the json database (required for karma system)
 const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('./db.json')
@@ -16,10 +16,12 @@ const bot = new Discord.Client()
 const CommandHandler = require('./lib/CommandHandler')
 const KarmaVoting = require('./lib/KarmaCommands').KarmaVoting
 const KarmaRankings = require('./lib/KarmaCommands').KarmaRankings
+const Emotes = require('./lib/EmoteCommand')
 
 const handler = new CommandHandler(bot)
 handler.register(new KarmaVoting(db))
 handler.register(new KarmaRankings(db))
+handler.register(new Emotes(path.resolve('./config/emotes')))
 
 // log that we signed in
 bot.on('ready', () => {
